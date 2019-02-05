e811581c2d71624bef33ab0d5707535919d05c3b
Ryo Konishi <todoroki.auone@gmail.com>
2019-02-05 19:33:48 +0900

    標準入力から値を追加するようにする

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
+#![feature(slice_patterns)]
+
 use bptree::BPlusTree;
+use std::io;
 
 fn main() {
    // N=5のB+-Treeを作る
    let mut tree = BPlusTree::new(5);
 
-   println!("{}", tree);
+   println!("Enter key and value! eg. `1 100`");
 
-   for i in 1..=20 {
-      tree.insert(i, i);
-      println!("{}", tree);
-   }
+   loop {
+      let mut input_text = String::new();
+      io::stdin()
+         .read_line(&mut input_text)
+         .expect("failed to read from stdin");
 
-   // もう一度初期化，今度は逆順で数字を入れる
-   let mut tree = BPlusTree::new(5);
-
-   println!("{}", tree);
-
-   for i in (1..=20).rev() {
-      tree.insert(i, i);
-      println!("{}", tree);
+      let words: Vec<&str> = input_text.trim().split(' ').collect();
+      let numbers: Option<Vec<Result<usize, std::num::ParseIntError>>> = words
+         .get(..)
+         .map(|args| args.iter().map(|arg| arg.parse()).collect());
+      if let Some(num_vec) = &numbers {
+         match &num_vec[..] {
+            &[Ok(key)] => {
+               tree.insert(key, key);
+               println!("{}", tree);
+            },
+            &[Ok(key), Ok(value), ..] => {
+               tree.insert(key, value);
+               println!("{:#?}", tree);
+            },
+            _ => println!("failed to parse key value"),
+         };
+      }
    }
 }
